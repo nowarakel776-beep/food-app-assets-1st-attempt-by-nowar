7208d3d1f1109b4917ae27a35dff6e08689ec4ab
nowarakel776-beep <nowarakel776@gmail.com>
2026-09-18 00:52:01 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const admin = require('firebase-admin').default || require('firebase-admin');
+const admin = require('firebase-admin');
 const cors = require('cors');
 
 const app = express();
