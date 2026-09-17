ab722ace83543a46170107c2bdae51ab83990531
nowarakel776-beep <nowarakel776@gmail.com>
2026-09-18 01:37:50 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,13 +5,16 @@ const cors = require('cors');
 const app = express();
 app.use(express.json());
 app.use(cors());
-
+let privateKey = process.env.FIREBASE_PRIVATE_KEY;
+if(privateKey){
+  privateKey = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n');
+}
 // Initialize Firebase Admin using environment variables
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') : undefined,
+    privateKey: privateKey,
   }),
 });
 
